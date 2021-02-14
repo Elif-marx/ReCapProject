adb009310a16e56fd06a55588f42511d27304c0e
Elif-marx <elifnazkaya_01@hotmail.com>
2021-02-14 18:33:26 +0300

    10.gün

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -9,26 +10,53 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ListCars();
-            
             
             CarManager carManager = new CarManager(new EfCarDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            CarTest();
+            //ListCars();
+            //BrandTest();
+            //foreach (var car in carManager.GetCarDetails().Data)
+            //{
+            //  Console.WriteLine(car.CarName+" marka  "+car.BrandName);
+            //}
 
-            foreach (var car in carManager.GetCarDetails())
+            void CarTest()
             {
-              Console.WriteLine(car.CarName+" marka  "+car.BrandName);
+                CarManager carmanager = new CarManager(new EfCarDal());
+                var result = carManager.GetCarDetails();
+                if (result.Success==true)
+                {
+                    foreach (var car in result.Data)
+                    {
+                        Console.WriteLine(car.Id + "-" + car.CarName + "-" + car.BrandName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(result.Message);
+                }
+
             }
             
+        }
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
         }
 
         private static void ListCars()
         {
             CarManager carManager = new CarManager(new EfCarDal());
-
-            foreach (var car in carManager.GetAll())//getall()
+            foreach (var car in carManager.GetAll().Data)
             {
                 Console.WriteLine(car.Description + "---Marka:" + car.BrandName + "---Renk:" + car.ColorName + "---Günlük Fiyat:" + car.DailyPrice);
             }
         }
     }
-}//Product productToDelete=null;//=new product; dersek gereksiz yere belleği yorarız referans atanır LINQ
+}
